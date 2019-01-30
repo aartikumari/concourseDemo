a9a7360d40aba38357cdae5b17c6523222847f9f
aartikumari <anandkrishna.1988@gmail.com>
2019-01-30 15:31:42 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "Hello world"
 
+cd concourseDemo
+fly -t target login -c $CONCOURSE_URL --username $USERNAME --password $PASSWORD
+echo "Test1"
+yes | fly -t target set-pipeline -c <(cat "ci/resource.yml" "ci/pipeline.yml") --load-vars-from credentials.yml 
+
+echo "Configure pipeline"
